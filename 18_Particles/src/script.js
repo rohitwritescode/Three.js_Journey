eddaeefb246cab32e92235b03bcbb10ade9e2b15
Rohit Bhat <rohitwritescode@gmail.com>
2022-01-26 22:22:23 -0800

    Completed lesson 18 on particles.

--- 18_Particles/src/script.js
+++ 18_Particles/src/script.js
@@ -19,15 +19,56 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particlesTexture = textureLoader.load('/textures/particles/1.png')
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+// Particles
+
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry(1, 32, 32)
+const count = 2000
+
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+
+for (let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions, 3)
 )
-scene.add(cube)
+
+particlesGeometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(colors, 3)
+)
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.1,
+    sizeAttenuation: true,
+    // color: new THREE.Color('#ffffff'),
+    transparent: true,
+    alphaMap: particlesTexture,
+    // alphaTest: 0.001
+    // depthTest: false
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    vertexColors: true
+})
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+// Cube
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(),
+//     new THREE.MeshBasicMaterial({color: 'red'})
+// )
+// scene.add(cube)
 
 /**
  * Sizes
@@ -84,6 +125,16 @@ const tick = () =>
 
     // Update controls
     controls.update()
+
+    // Update particles
+    // particles.rotation.y = elapsedTime * 0.1
+    for(let i = 0; i < count; i++) {
+        const i3 = i * 3
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+
+    particlesGeometry.attributes.position.needsUpdate = true
 
     // Render
     renderer.render(scene, camera)
