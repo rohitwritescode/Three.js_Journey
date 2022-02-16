9d72ae2a72ccaa80b6e38452a05ebf4b36c549f3
Rohit Bhat <rohitwritescode@gmail.com>
2022-02-15 23:02:31 -0800

    Completed lesson 26 on code structuring for bigger projects.

--- 26_Code-Structuring-For-Bigger-Projects/src/Experience/Experience.js
+++ 26_Code-Structuring-For-Bigger-Projects/src/Experience/Experience.js
@@ -6,6 +6,7 @@ import Renderer from './Renderer'
 import World from './World/World'
 import Resources from './Utils/Resources'
 import sources from './sources'
+import Debug from './Utils/Debug'
 
 let instance = null
 
@@ -23,6 +24,7 @@ export default class Experience {
         this.canvas = canvas
 
         // Setup
+        this.debug = new Debug()
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
@@ -49,6 +51,39 @@ export default class Experience {
 
     update() {
         this.camera.update()
+        this.world.update()
         this.renderer.update()
+    }
+
+    destroy() {
+        this.sizes.off('resize')
+        this.time.off('tick')
+
+        // Traverse the entire scene
+        this.scene.traverse((child) => {
+            
+            if(child instanceof THREE.Mesh)
+            {
+                child.geometry.dispose()
+
+                // Loop through the material properties
+                for(const key in child.material)
+                {
+                    const value = child.material[key]
+
+                    // Test if there is a dispose function
+                    if(value && typeof value.dispose === 'function')
+                    {
+                        value.dispose()
+                    }
+                }
+            }
+        })
+
+        this.camera.controls.dispose()
+        this.renderer.instance.dispose()
+        if(this.debug.active) {
+            this.debug.ui.destroy()
+        }
     }
 }
--- 26_Code-Structuring-For-Bigger-Projects/src/Experience/World/World.js
+++ 26_Code-Structuring-For-Bigger-Projects/src/Experience/World/World.js
@@ -1,6 +1,7 @@
-import * as THREE from 'three'
 import Experience from "../Experience";
 import Environment from './Environment';
+import Floor from './Floor';
+import Fox from './Fox';
 
 
 export default class World {
@@ -9,16 +10,17 @@ export default class World {
         this.scene = this.experience.scene
         this.resources = this.experience.resources
 
-        // Test Mesh        
-        const testMesh = new THREE.Mesh(
-            new THREE.BoxGeometry,
-            new THREE.MeshStandardMaterial()
-        )
-        this.scene.add(testMesh)
-
         this.resources.on('ready', () => {
             // Environment
-            this.environment = new Environment()            
+            this.floor = new Floor()
+            this.fox = new Fox()
+            this.environment = new Environment()
         })
+    }
+
+    update() {
+        if(this.fox) {
+            this.fox.update()
+        }
     }
 }
