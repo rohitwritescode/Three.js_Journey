d7d149669a79bca9fc211666ab658f800368e5ee
Rohit Bhat <rohitwritescode@gmail.com>
2022-02-16 16:39:41 -0800

    Added simple GLSL code to vertex.glsl for practice.

--- 27_Shaders/src/shaders/test/vertex.glsl
+++ 27_Shaders/src/shaders/test/vertex.glsl
@@ -5,5 +5,8 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 
 void main() {
+    vec4 foo = vec4(1.0, 2.0, 3.0, 4.0);
+    float bar = foo.w
+
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
