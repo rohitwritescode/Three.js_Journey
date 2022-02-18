c0965e6046344f38885003615dbcb3ada8c269c1
Rohit Bhat <rohitwritescode@gmail.com>
2022-02-17 21:58:32 -0800

    Completed lesson 27 on shaders.

--- 27_Shaders/src/shaders/test/fragment.glsl
+++ 27_Shaders/src/shaders/test/fragment.glsl
@@ -1,5 +1,15 @@
-precision mediump float;
+// precision mediump float;
+
+uniform vec3 uColor;
+uniform sampler2D uTexture;
+
+// varying float vRandom;
+varying float vElevation;
+varying vec2 vUv;
 
 void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    // gl_FragColor = vec4(vRandom, vRandom * 0.5, 1.0, 1.0);
+    vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb *= vElevation * 2.0 + 0.9;
+    gl_FragColor = textureColor;
 }
--- 27_Shaders/src/shaders/test/vertex.glsl
+++ 27_Shaders/src/shaders/test/vertex.glsl
@@ -1,12 +1,35 @@
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelMatrix;
+// uniform mat4 projectionMatrix;
+// uniform mat4 viewMatrix;
+// uniform mat4 modelMatrix;
+uniform vec2 uFrequency;
+uniform float uTime;
 
-attribute vec3 position;
+// attribute vec3 position;
+attribute float aRandom;
+// attribute vec2 uv;
+
+varying float vRandom;
+varying float vElevation;
+varying vec2 vUv;
 
 void main() {
-    vec4 foo = vec4(1.0, 2.0, 3.0, 4.0);
-    float bar = foo.w
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    
+    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+    modelPosition.z += elevation;
 
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    // modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    // modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+    vUv = uv;
+    vElevation = elevation;
+
+    // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    // modelPosition.z += aRandom * 0.1;
+    // vRandom = aRandom;    
 }
